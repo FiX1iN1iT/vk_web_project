c21d6dab5fa367ab8e7a09a454913070a776b021
fix1in1it <fix1in1it@gmail.com>
2023-10-26 23:21:40 +0300

    1:28:10 sem2

--- app/views.py
+++ app/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+def index(request):
+    questions = [
+        {
+            'id': i,
+            'title': f'Question {i}',
+            'content': f'Long lorem ipsum {i}'
+        } for i in range(20)
+    ]
+
+    return render(request, 'index.html', {'questions': questions})
+
+
+def question(request):
+    return render(request, 'question.html')
