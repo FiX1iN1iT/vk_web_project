345320bad19faaf5112619fd8b8d0e156a559a6e
fix1in1it <fix1in1it@gmail.com>
2023-11-07 20:43:58 +0300

    dz2 i guess

--- app/models.py
+++ app/models.py
@@ -1,3 +1,59 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    # photo = models.ImageField(upload_to="avatars/", null=True, blank=True)
+
+    def __str__(self):
+        return self.user.username
+
+
+class Vote(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    question = models.ForeignKey('Question', on_delete=models.CASCADE, null=True, blank=True)
+    answer = models.ForeignKey('Answer', on_delete=models.CASCADE, null=True, blank=True)
+    value = models.IntegerField()
+
+    def __str__(self):
+        return f"'{self.user.username}' voted on '{self.question.title}'"
+
+
+class QuestionManager(models.Manager):
+    def tagged(self, tag_name):
+        return self.filter(tags__name=tag_name)
+
+
+class Question(models.Model):
+    user = models.ForeignKey(User, on_delete=models.PROTECT)
+    title = models.CharField(max_length=256)
+    content = models.TextField()
+    tags = models.ManyToManyField('Tag', related_name='questions')
+
+    objects = QuestionManager()
+
+    def __str__(self):
+        return self.title
+
+
+class Answer(models.Model):
+    user = models.ForeignKey(User, on_delete=models.PROTECT)
+    question = models.ForeignKey('Question', on_delete=models.CASCADE, related_name='answers')
+    content = models.TextField()
+
+    STATUS_CHOICES = [
+        ('c', 'correct'),
+        ('i', 'incorrect'),
+    ]
+    status = models.CharField(max_length=1, choices=STATUS_CHOICES, default='i')
+
+    def __str__(self):
+        return f"Answer to '{self.question.title}' from '{self.user.username}'"
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=256, unique=True)
+
+    def __str__(self):
+        return self.name
--- app/views.py
+++ app/views.py
@@ -1,27 +1,85 @@
 from django.shortcuts import render
 from django.core.paginator import Paginator
+from django.http import Http404
+
+from app.models import Profile
+from app.models import Question
+from app.models import Answer
+from app.models import Tag
+from app.models import Vote
 
 QUESTION = [
     {
         'id': i,
         'title': f'Question {i}',
         'content': f'Long lorem ipsum {i}'
-    } for i in range(20)
+    } for i in range(40)
+]
+
+ANSWER = [
+    {
+        'id': i,
+        'title': f'Answer {i}',
+        'content': f'Long lorem ipsum {i}'
+    } for i in range(40)
+]
+
+TAG = [
+    {
+        'id': i,
+        'name': f'Tag {i}',
+    } for i in range(10)
 ]
 
 
-def paginate(objects, page, per_page=15):
-    paginator = Paginator(objects, per_page)
-
-    return paginator.page(page)
+def paginate(objects_list, request, per_page=5):
+    paginator = Paginator(objects_list, per_page)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    return page_obj
 
 
 # Create your views here.
 def index(request):
-    page = request.GET.get('page', 1)
-    return render(request, 'index.html', {'questions': paginate(QUESTION, page)})
+    context = {
+        'page_obj': paginate(QUESTION, request),
+    }
+    return render(request, 'index.html', context)
+
+
+def login(request):
+    return render(request, 'login.html')
+
+
+def ask(request):
+    return render(request, 'ask.html')
+
+
+def signup(request):
+    return render(request, 'signup.html')
+
+
+def hot(request):
+    context = {
+        'page_obj': paginate(QUESTION, request),
+    }
+    return render(request, 'hot.html', context)
+
+
+def tag(request, tag_name):
+    questions = Question.objects.tagged(tag_name)
+    context = {
+        'tag': tag_name,
+        'page_obj': paginate(questions, request),
+    }
+    return render(request, 'tag.html', context)
 
 
 def question(request, question_id):
-    item = QUESTION[question_id]
-    return render(request, 'question.html', {'question': item})
+    if question_id >= len(QUESTION):
+        raise Http404("Question does not exist")
+    context = {
+        'question': QUESTION[question_id],
+        'page_obj': paginate(ANSWER, request),
+    }
+    return render(request, 'question.html', context)
