e2398a997330789d78030f36e4fea4f7cde58fd7
fix1in1it <fix1in1it@gmail.com>
2023-11-13 15:57:28 +0300

    cmd ready

--- app/management/commands/fill_db.py
+++ app/management/commands/fill_db.py
@@ -1,5 +1,6 @@
 from django.core.management import BaseCommand
 from faker import Faker
+from django.contrib.auth.models import User
 
 from app.models import Question, Answer, Profile, Tag, Vote
 
@@ -7,29 +8,74 @@ fake = Faker()
 
 
 class Command(BaseCommand):
-    help = "Fills database with fake data"
+    help = "Fills database with fake data for your models"
 
     def add_arguments(self, parser):
         parser.add_argument("num", type=int)
 
     def handle(self, *args, **kwargs):
         num = kwargs['num']
-        authors = [
-            Author(
-                name=fake.first_name(), surname=fake.last_name(),
-                birth_date=str(fake.date_between(start_date='-100y', end_date='-20y')),
-                death_date=None if (fake.random_int() % 2 == 0) else
-                str(fake.date_between(start_date='-19y', end_date='-1d'))
-            ) for _ in range(num)
+        votes_amount = num * 200
+        questions_amount = num * 10
+        answers_amount = num * 100
+
+        # Create fake user profiles
+        profiles = [
+            Profile(user=User(username=fake.user_name(), email=fake.email()))
+            for _ in range(num)
         ]
-        Author.objects.bulk_create(authors)
-        authors = Author.objects.all()
-        authors_count = authors.count()
-        books = [
-            Book(
+        Profile.objects.bulk_create(profiles)
+
+        # Create fake tags
+        tags = [
+            Tag(name=fake.word())
+            for _ in range(num)
+        ]
+        Tag.objects.bulk_create(tags)
+
+        profiles = Profile.objects.all()
+        tags = Tag.objects.all()
+
+        # Create fake votes
+        votes = []
+        for _ in range(votes_amount):
+            flag = fake.boolean()
+            user = profiles[fake.random_int(min=0, max=num - 1)].user
+            question = None if flag else Question.objects.order_by('?').first()
+            answer = None if not flag else Answer.objects.order_by('?').first()
+            value = fake.random_element(elements=(-1, 1))
+
+            votes.append(Vote(user, question, answer, value))
+
+        Vote.objects.bulk_create(votes)
+
+        # Create fake questions
+        questions = [
+            Question(
+                user=profiles[fake.random_int(min=0, max=num - 1)].user,
                 title=fake.sentence(nb_words=3),
-                author=authors[fake.random_int(min=0, max=authors_count - 1)],
-                date_written=str(fake.date_between(start_date='-40y', end_date='-10y'))
-            ) for _ in range(num*2)
+                content=fake.paragraph(),
+                created_at=fake.date_between(start_date='-1y', end_date='today')
+            ) for _ in range(questions_amount)
         ]
-        Book.objects.bulk_create(books)
+        Question.objects.bulk_create(questions)
+
+        # Create fake answers
+        answers = []
+        for _ in range(answers_amount):
+            user = profiles[fake.random_int(min=0, max=num - 1)].user
+            question = Question.objects.order_by('?').first()
+            content = fake.paragraph()
+            created_at = fake.date_between(
+                start_date=question.created_at,
+                end_date='today'
+            )
+            status = fake.random_element(elements=('c', 'i'))
+
+            answers.append(Answer(user, question, content, created_at, status))
+
+        Answer.objects.bulk_create(answers)
+
+        # Add tags to questions
+        for question in Question.objects.all():
+            question.tags.set(tags.order_by('?')[:fake.random_int(min=1, max=6)])
--- app/models.py
+++ app/models.py
@@ -1,5 +1,22 @@
 from django.db import models
 from django.contrib.auth.models import User
+from datetime import date
+
+
+class QuestionManager(models.Manager):
+    def tagged(self, tag_name):
+        return self.filter(tags__name=tag_name)
+
+    def total_votes(self, question_id):
+        return Vote.objects.filter(question_id=question_id).aggregate(models.Sum('value'))['value__sum'] or 0
+
+    def get_answer_count(self, question_id):
+        return self.get(pk=question_id).answers.count()
+
+
+class AnswerManager(models.Manager):
+    def total_votes(self, answer_id):
+        return Vote.objects.filter(answer_id=answer_id).aggregate(models.Sum('value'))['value__sum'] or 0
 
 
 class Profile(models.Model):
@@ -20,16 +37,12 @@ class Vote(models.Model):
         return f"'{self.user.username}' voted on '{self.question.title}'"
 
 
-class QuestionManager(models.Manager):
-    def tagged(self, tag_name):
-        return self.filter(tags__name=tag_name)
-
-
 class Question(models.Model):
     user = models.ForeignKey(User, on_delete=models.PROTECT)
     title = models.CharField(max_length=256)
     content = models.TextField()
     tags = models.ManyToManyField('Tag', related_name='questions')
+    created_at = models.DateField(default=date(2003, 12, 1))
 
     objects = QuestionManager()
 
@@ -41,12 +54,15 @@ class Answer(models.Model):
     user = models.ForeignKey(User, on_delete=models.PROTECT)
     question = models.ForeignKey('Question', on_delete=models.CASCADE, related_name='answers')
     content = models.TextField()
+    created_at = models.DateField(default=date(2003, 12, 1))
 
     STATUS_CHOICES = [
         ('c', 'correct'),
         ('i', 'incorrect'),
     ]
     status = models.CharField(max_length=1, choices=STATUS_CHOICES, default='i')
+
+    objects = AnswerManager()
 
     def __str__(self):
         return f"Answer to '{self.question.title}' from '{self.user.username}'"
